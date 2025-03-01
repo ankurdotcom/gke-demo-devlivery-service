7fba448cb5fcc1fcf22a99b2f82ebbe369d145d3
Ankur Gupta <ankurdotcom@gmail.com>
2025-03-01 14:36:18 +0530

    first commit

---
+++ src/main/java/demo/gke/ecom/devliveryservice/DevliveryserviceApplication.java
@@ -0,0 +1,13 @@
+package demo.gke.ecom.devliveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevliveryserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevliveryserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demo/gke/ecom/devliveryservice/DevliveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.gke.ecom.devliveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevliveryserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
